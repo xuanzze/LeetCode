fcd068bd9899fee39946eb000d2726ade516c08d
xuanzze <46557598+xuanzze@users.noreply.github.com>
2019-01-11 15:56:46 +0800

    Create 134. Gas Station.java

---
+++ Problems/134. Gas Station.java
@@ -0,0 +1,10 @@
+public int canCompleteCircuit(int[] gas, int[] cost) {
+    int tank = 0, total = 0, start = 0;
+    for (int i = 0; i < gas.length; i++) 
+        if ((tank = tank + gas[i] - cost[i]) < 0) {
+            start = i + 1;
+            total += tank;
+            tank = 0;
+        }
+    return total + tank < 0 ? -1: start;
+}
