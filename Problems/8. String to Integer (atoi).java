28e25145907e386799472a602a62e031f475b4d9
xuanzze <46557598+xuanzze@users.noreply.github.com>
2019-01-18 23:07:29 +0800

    Create 8. String to Integer (atoi).java

---
+++ Problems/8. String to Integer (atoi).java
@@ -0,0 +1,16 @@
+public int myAtoi(String str) {
+    if (str.isEmpty())  return 0;
+    int sign = 1, base = 0, i = 0;
+    while (i < str.length() && str.charAt(i) == ' ')
+        i++;
+    if (i >= str.length())
+        return 0;
+    if (str.charAt(i) == '-' || str.charAt(i) == '+')
+        sign = str.charAt(i++) == '-'? -1: 1;
+    while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+        if (base > Integer.MAX_VALUE / 10 || (base == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > 7))
+            return (sign == 1)? Integer.MAX_VALUE: Integer.MIN_VALUE;
+        base = base * 10 + str.charAt(i) - '0';
+    }
+    return base * sign;
+}
