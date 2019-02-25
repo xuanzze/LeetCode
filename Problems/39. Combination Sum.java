1b27fca0fa7a2041388dfb60644704f24d5c76d4
xuanzze <46557598+xuanzze@users.noreply.github.com>
2019-02-25 15:44:43 +0800

    Create 39. Combination Sum.java

---
+++ Problems/39. Combination Sum.java
@@ -0,0 +1,18 @@
+public List<List<Integer>> combinationSum(int[] nums, int target) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, target, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+    if(remain < 0) return;
+    else if(remain == 0) list.add(new ArrayList<>(tempList));  // 转换用法
+    else{
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, remain - nums[i], i);  // 不用i + 1是因为可用重复元素
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
