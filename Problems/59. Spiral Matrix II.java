4b61f0a3ca4d822d4475b441dc5c3ecb7aed2880
xuanzze <46557598+xuanzze@users.noreply.github.com>
2019-03-08 14:25:32 +0800

    Create 59. Spiral Matrix II.java

---
+++ Problems/59. Spiral Matrix II.java
@@ -0,0 +1,24 @@
+public int[][] generateMatrix(int n) {
+    int[][] res = new int[n][n];
+    int rowBegin = 0, rowEnd = n - 1;
+    int colBegin = 0, colEnd = n - 1;
+    int num = 1;
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+        for (int i = colBegin; i <= colEnd; i++)
+            res[rowBegin][i] = num++;
+        rowBegin++;
+
+        for (int i = rowBegin; i <= rowEnd; i++)
+            res[i][colEnd] = num++;
+        colEnd--;
+
+        for (int i = colEnd; i >= colBegin; i--)
+            res[rowEnd][i] = num++;
+        rowEnd--;
+
+        for (int i = rowEnd; i >= rowBegin; i--)
+            res[i][colBegin] = num++;
+        colBegin++;
+    }
+    return res;
+}
