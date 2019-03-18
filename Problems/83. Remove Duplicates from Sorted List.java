8543d51ba77f2fddcd10a519da0f643706b70423
xuanzze <46557598+xuanzze@users.noreply.github.com>
2019-03-18 09:37:23 +0800

    Create 83. Remove Duplicates from Sorted List.java

---
+++ Problems/83. Remove Duplicates from Sorted List.java
@@ -0,0 +1,14 @@
+public ListNode deleteDuplicates(ListNode head) {
+    ListNode cur = head;
+
+    while(cur != null) {
+        if (cur.next == null) {
+            break;
+        }
+        if (cur.val == cur.next.val) 
+            cur.next = cur.next.next;
+        else
+            cur = cur.next;
+    }
+    return head;
+}
