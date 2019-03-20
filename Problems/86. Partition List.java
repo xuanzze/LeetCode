75c72755bf0155dba40db91fd2967a546ea157fa
xuanzze <46557598+xuanzze@users.noreply.github.com>
2019-03-20 13:18:56 +0800

    Create 86. Partition List.java

---
+++ Problems/86. Partition List.java
@@ -0,0 +1,14 @@
+public ListNode partition(ListNode head, int x) {
+    ListNode smallerHead = new ListNode(0), biggerHead = new ListNode(0);
+    ListNode smaller = smallerHead, bigger = biggerHead;
+    while (head != null) {
+        if (head.val < x) 
+            smaller = smaller.next = head;
+        else 
+            bigger = bigger.next = head;
+        head = head.next;
+    }
+    smaller.next = biggerHead.next;
+    bigger.next = null;
+    return smallerHead.next;
+}
