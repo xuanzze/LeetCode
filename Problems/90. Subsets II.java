502e0aad841f4dfb20bbf52851a15d1c0d9ac117
xuanzze <46557598+xuanzze@users.noreply.github.com>
2019-03-23 08:59:27 +0800

    Create 90. Subsets II.java

---
+++ Problems/90. Subsets II.java
@@ -0,0 +1,23 @@
+public List<List<Integer>> subsetsWithDup(int[] num) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    List<Integer> empty = new ArrayList<Integer>();
+    result.add(empty);
+    Arrays.sort(num);
+
+    for (int i = 0; i < num.length; i++) {
+        int dupCount = 0;
+        while (((i + 1) < num.length) && num[i + 1] == num[i]) {
+            dupCount++;
+            i++;
+        }
+        int prevNum = result.size();
+        for (int j = 0; j < prevNum; j++) {
+            List<Integer> element = new ArrayList<Integer>(result.get(j));
+            for (int t = 0; t <= dupCount; t++) {   // 这里是小于等于，至少有一次添加
+                element.add(num[i]);
+                result.add(new ArrayList<Integer>(element));
+            }
+        }
+    }
+    return result;
+}
