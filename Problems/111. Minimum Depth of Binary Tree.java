84dce637abe60440855b7e480303f97bd3c5c84e
xuanzze <46557598+xuanzze@users.noreply.github.com>
2019-04-01 10:27:32 +0800

    Create 111. Minimum Depth of Binary Tree.java

---
+++ Problems/111. Minimum Depth of Binary Tree.java
@@ -0,0 +1,6 @@
+public int minDepth(TreeNode root) {
+	if (root == null)   return 0;
+	int left = minDepth(root.left);
+	int right = minDepth(root.right);
+	return (left != 0 && right != 0) ? Math.min(left, right) + 1: left + right + 1;
+}
